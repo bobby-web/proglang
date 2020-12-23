56a8aaf1e2f6c992bb3d4819cf98cf45de6e473c
bobby-web <55195746+bobby-web@users.noreply.github.com>
2020-12-23 08:04:43 +0700

    Update constant.c

--- c/1.BasicConcept/constant.c
+++ c/1.BasicConcept/constant.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-// Constant Declared And Define Inside Main Function
+// Constant Declared And Define Inside Main
 int main (void) {
     const double PI =3.14;
     printf("%f",PI);
